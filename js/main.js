54bacf8e6f58845dd40a1d690f4c2828363680bb
Ayelen Machicado <ayelenmachicado2001@gmail.com>
2023-05-13 23:48:47 -0300

    Segunda Reentrega - Correccion

--- js/main.js
+++ js/main.js
@@ -4,82 +4,63 @@ let saludoInicial = alert("Bienvenido a BoleteríaROCK, la tienda virtual para c
 
 let seleccion = parseInt(prompt("Entradas disponibles para las siguientes bandas:\n 0) Fito Paez \n 1) Luis Spinetta \n 2) Charly García \n 3) Soda Estereo"));
 
-let entrada = parseInt(prompt("Cuantas entradas quiere comprar?"));
+let entrada = parseInt(prompt("Cuántas entradas quiere comprar?"));
 
 
 let fitoPaez = {
     nombre: "Fito Paez",
     fecha: "04-07-1999",
-    lugar: "Recinto",
+    lugar: "Movistar Arena",
     precio: 3800
 };
 
 let luisSpinetta = {
     nombre: "Luis Spinetta",
     fecha: "10-09-2001",
-    lugar: "Recinto",
+    lugar: "Teatro Colón",
     precio: 4500,
 };
 
 let charlyGarcia = {
     nombre: "Charly García",
     fecha: "18-11-1999",
-    lugar: "Recinto",
+    lugar: "Luna Park",
     precio: 2900,
 };
 
 let sodaEstereo = {
     nombre: "Soda Estereo",
     fecha: "22-08-2002",
-    lugar: "Recinto",
+    lugar: "Teatro Gran Rex",
     precio: 5200,
 };
 
-
-
 const artistas = [fitoPaez, luisSpinetta, charlyGarcia, sodaEstereo];
 
-if(seleccion === 0){
-    let valor = fitoPaez["precio"];
-    cuenta();
-} else if(seleccion === 1){
-    let valor = luisSpinetta["precio"];
-    cuenta();
-} else if(seleccion === 2){
-    let valor = charlyGarcia["precio"];
-    cuenta();
-} else if(seleccion === 3){
-    let valor = sodaEstereo["precio"];
-    cuenta();
-}
-
-function cuenta(precio){
-    let resultado = entrada * precio;
-    console.log(resultado);
-}
-
-console.log("DATOS DEL CONCIERTO");
+console.log("DATOS DEL CONCIERTO:");
 
 for (let i = 0; i < artistas.length; i++) {
   artistas[i].nombre = artistas[i].nombre.toUpperCase();
   artistas[i].lugar = artistas[i].lugar.toUpperCase();
 }
 
-console.log(artistas[seleccion]);
-console.log("TOTAL A PAGAR:");
+console.log(
+"ARTISTA: " + artistas[seleccion].nombre 
++ "\n" + "CUANDO: " + artistas[seleccion].fecha 
++ "\n" + "DONDE: " + artistas[seleccion].lugar
++ "\n" + "PRECIO: $ " + artistas[seleccion].precio);
 
-let valor = 0;
+console.log("Cantidad de entradas: " + entrada);
 
-if(seleccion === 0){
-    let valor = fitoPaez["precio"];
-    cuenta(valor);
-} else if(seleccion === 1){
-    let valor = luisSpinetta["precio"];
-    cuenta(valor);
-} else if(seleccion === 2){
-    let valor = charlyGarcia["precio"];
-    cuenta(valor);
-} else if(seleccion === 3){
-    let valor = sodaEstereo["precio"];
-    cuenta(valor);
+let precio = artistas[seleccion].precio;
+function calcular(precio, entrada)
+{
+    resultado2 = entrada * precio;
+    return resultado2;
 }
+
+console.log("TOTAL A PAGAR: $ " + calcular(precio, entrada));
+
+restantesArtistas = [artistas[0].nombre, artistas[1].nombre, artistas[2].nombre, artistas[3].nombre];
+artistaEliminado = restantesArtistas.splice(seleccion, 1);
+console.log("ENTRADAS DISPONIBLES PARA: " + restantesArtistas);
